9f1cc299b625132606388118638e579a63f76542
Grant Allen <gallen101@gmail.com>
2026-04-28 13:40:45 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,33 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1, 100);
 
-        Console.WriteLine($"The magic number is {number}.");
+        string playgame = "yes";
 
-        int guess = 0;
-
-        while (guess != number)
+        while (playgame.ToLower() == "yes")
         {
-            Console.Write("What is your guess? ");
-            guess = int.Parse(Console.ReadLine());
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 100);
 
-            if (guess > number)
+            Console.WriteLine($"The magic number is {number}.");
+
+            int guess = 0;
+            int guessnumber = 0;
+
+            while (guess != number)
             {
-                Console.WriteLine("Lower");
-            }
-            else if (guess < number)
-            {
-                Console.WriteLine("Higher");
-            }
-            else
-            {
-                Console.WriteLine("You Guessed it!!");
+                guessnumber += 1;
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+                if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("You Guessed it!!");
+                }
+
+                Console.WriteLine();
             }
 
+            Console.WriteLine($"You guessed {guessnumber} times."); 
+             
             Console.WriteLine();
+
+            Console.Write("Would you like to play again (Yes/No)? ");
+            playgame = Console.ReadLine();
+
         }
+
+
 
     }
 }
