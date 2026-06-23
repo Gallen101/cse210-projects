d8a8abd2582e884f6fbf8c68d4993bbea5c0b656
Grant Allen <gallen101@gmail.com>
2026-06-23 09:15:24 -0600

    "Finished learning 5"

---
+++ prepare/Learning05/Square.cs
@@ -0,0 +1,16 @@
+using System.Diagnostics.Contracts;
+
+public class Square : Shape
+{
+    private double _side;
+
+    public Square(string color, double side) : base (color)
+    {
+        _side = side;
+    }
+
+    public override double Getarea()
+    {
+        return _side * _side;
+    }
+}
